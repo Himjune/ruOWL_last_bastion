cdb0a4a710c5cbe2dc6076a9ba496db8876f9e94
Himjune <himjune@mail.ru>
2020-02-14 00:31:34 +0300

    tw_ready flag

--- tw_scripts.js
+++ tw_scripts.js
@@ -9,13 +9,17 @@ let props = {
 
 var embed = new Twitch.Embed("twPlayer", props);
 var g_player;
+var player_ready = false;
 embed.addEventListener(Twitch.Embed.VIDEO_READY, () => {
   g_player = embed.getPlayer();
   g_player.play();
+  player_ready = true;
 });
 
 setInterval(() => {
-  document.getElementById("twVol").innerText = g_player.getVolume();
-  document.getElementById("twCur").innerText = g_player.getCurrentTime();
-  document.getElementById("twDur").innerText = g_player.getDuration();
+  if (player_ready) {
+    document.getElementById("twVol").innerText = g_player.getVolume();
+    document.getElementById("twCur").innerText = g_player.getCurrentTime();
+    document.getElementById("twDur").innerText = g_player.getDuration();
+  }
 }, 500);
