fc3e852ab833535d156a8848660dbbcd1ebfe726
Himjune <himjune@mail.ru>
2020-02-14 19:13:44 +0300

    schemeBtns fixes

--- yt_scripts.js
+++ yt_scripts.js
@@ -23,45 +23,46 @@ function onYouTubeIframeAPIReady() {
 }
 
 // 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-  event.target.playVideo();
-}
 
 
 var last_fixed_end_time = -1.0;
 var cur_end_time = -1.0;
 var fixed_delay = 9.0;
-setInterval(function () {
+function onPlayerReady(event) {
+  event.target.playVideo();
 
-  document.getElementById("fraction").innerText = last_fixed_end_time;
-  document.getElementById("ctime").innerText = yt_player.getCurrentTime();
-  document.getElementById("cend").innerText = cur_end_time;
-  document.getElementById("del").innerText = fixed_delay;
-  document.getElementById("cspe").innerText = yt_player.getPlaybackRate();
-  document.getElementById("gdur").innerText = yt_player.getDuration();
-
-/*
-  if (yt_player.getCurrentTime() > 0 && last_fixed_end_time < 0) {
-    last_fixed_end_time = yt_player.getCurrentTime();
-    cur_end_time = last_fixed_end_time;
-  } else if (yt_player.getCurrentTime() < yt_player.getDuration() * 0.75) {
-    yt_player.seekTo(yt_player.getDuration(), false);
-    last_fixed_end_time = yt_player.getCurrentTime();
-    cur_end_time = last_fixed_end_time;
-  } else {
-    cur_end_time = cur_end_time + 1;
-  }
-
-  document.getElementById("ctar").innerText = (cur_end_time - fixed_delay);
-  let diff = yt_player.getCurrentTime() - cur_end_time + fixed_delay;
-
-  if (diff > 2) yt_player.setPlaybackRate(0.25);
-  else if (diff > 0.3) yt_player.setPlaybackRate(0.75);
-  else if (diff < -2) yt_player.setPlaybackRate(1.75);
-  else if (diff < -0.3) yt_player.setPlaybackRate(1.25);
-  else yt_player.setPlaybackRate(1);
-*/
-}, 1000);
+  setInterval(function () {
+  
+    document.getElementById("fraction").innerText = last_fixed_end_time;
+    document.getElementById("ctime").innerText = yt_player.getCurrentTime();
+    document.getElementById("cend").innerText = cur_end_time;
+    document.getElementById("del").innerText = fixed_delay;
+    document.getElementById("cspe").innerText = yt_player.getPlaybackRate();
+    document.getElementById("gdur").innerText = yt_player.getDuration();
+  
+  /*
+    if (yt_player.getCurrentTime() > 0 && last_fixed_end_time < 0) {
+      last_fixed_end_time = yt_player.getCurrentTime();
+      cur_end_time = last_fixed_end_time;
+    } else if (yt_player.getCurrentTime() < yt_player.getDuration() * 0.75) {
+      yt_player.seekTo(yt_player.getDuration(), false);
+      last_fixed_end_time = yt_player.getCurrentTime();
+      cur_end_time = last_fixed_end_time;
+    } else {
+      cur_end_time = cur_end_time + 1;
+    }
+  
+    document.getElementById("ctar").innerText = (cur_end_time - fixed_delay);
+    let diff = yt_player.getCurrentTime() - cur_end_time + fixed_delay;
+  
+    if (diff > 2) yt_player.setPlaybackRate(0.25);
+    else if (diff > 0.3) yt_player.setPlaybackRate(0.75);
+    else if (diff < -2) yt_player.setPlaybackRate(1.75);
+    else if (diff < -0.3) yt_player.setPlaybackRate(1.25);
+    else yt_player.setPlaybackRate(1);
+  */
+  }, 1000);
+}
 
 
 function seekVideo() {
