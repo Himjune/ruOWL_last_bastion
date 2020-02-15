f5e3d5586b7cc929125bf00c4cb08ffb96cf9ee5
Himjune <himjune@mail.ru>
2020-02-15 18:09:11 +0300

    if dor play

--- yt_scripts.js
+++ yt_scripts.js
@@ -80,8 +80,9 @@ function onPlayerStateChange(event) {
   // OnPlay
   if (event.data == 1) {
     event.target.seekTo(event.target.getDuration(),true);
-    event.target.playVideo();
-    
+    if (yt_player.getPlayerState() != 1) {
+      event.target.playVideo();
+    }
     if (fixed_end_time_ts === '') {
       fixed_end_time_ts = Date.now();
       fixed_end_time = yt_player.getCurrentTime();
