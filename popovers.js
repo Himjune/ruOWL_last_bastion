b01edd71cdd2877815078982f6b44bba0adc5f85
Himjune <himjune@mail.ru>
2020-02-16 21:00:47 +0300

    popovers init on site

--- popovers.js
+++ popovers.js
@@ -1,3 +1,16 @@
 function hide_popover (id_str) {
+  localStorage.setItem("seen_"+id_str,"true");
   document.getElementById(id_str).style.display = "none";
+
+  if (id_str === "chatHelp") {
+    toggle_chat();
+  }
+}
+
+var popovers = ["shcemeHelp","shiftHelp","chatHelp"];
+for (var i = 0; i < popovers.length; i++) {
+  let pop_id = popovers[i];
+  if (localStorage.getItem("seen_"+pop_id) !== "true") {
+    document.getElementById(pop_id).style.display = "block";
+  }
 }
