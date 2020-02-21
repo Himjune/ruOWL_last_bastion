3a925219d8d8118b67e19a2f314b847d8850457b
Himjune <himjune@mail.ru>
2020-02-21 16:37:02 +0300

    next owl

--- yt_scripts.js
+++ yt_scripts.js
@@ -11,8 +11,8 @@ function onYouTubeIframeAPIReady() {
   if (videoId === '') {
     //videoId = 'zPm6tpIupq0'; // my test
     //videoId = '5Tw5xhRsonc'; // news stream
-    //videoId = '9C3BjtlcxxM'; // next owl
-    videoId = 'oEfQawTuAbw' // owl highlights
+    videoId = 'KhHWT3giIlA'; // next owl
+    //videoId = 'oEfQawTuAbw' // owl highlights
   }
 
   yt_player = new YT.Player('ytPlayer', {
