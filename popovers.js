4a36da0293d5e71554465e25205cea62633a8024
Himjune <himjune@mail.ru>
2020-02-22 19:12:24 +0300

    drop popovers for everyone

--- popovers.js
+++ popovers.js
@@ -1,5 +1,7 @@
+const SEEN_VAL = "2202";
+
 function hide_popover (id_str) {
-  localStorage.setItem("seen_"+id_str,"true");
+  localStorage.setItem("seen_"+id_str,SEEN_VAL);
   document.getElementById(id_str).style.display = "none";
 
   if (id_str === "chatHelp") {
@@ -11,7 +13,7 @@ var popovers = ["shcemeHelp","shiftHelp","chatHelp","replaysHelp"];
 function show_popovers () {
   for (var i = 0; i < popovers.length; i++) {
     let pop_id = popovers[i];
-    if (localStorage.getItem("seen_"+pop_id) !== "true") {
+    if (localStorage.getItem("seen_"+pop_id) !== SEEN_VAL) {
       document.getElementById(pop_id).style.display = "block";
     }
   }
