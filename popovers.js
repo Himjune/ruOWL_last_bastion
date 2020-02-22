7c5b6373ce951c7ff11aeac3f9a3c0162876032e
Himjune <himjune@mail.ru>
2020-02-22 19:04:41 +0300

    showPopoversBtn

--- popovers.js
+++ popovers.js
@@ -8,9 +8,22 @@ function hide_popover (id_str) {
 }
 
 var popovers = ["shcemeHelp","shiftHelp","chatHelp","replaysHelp"];
-for (var i = 0; i < popovers.length; i++) {
-  let pop_id = popovers[i];
-  if (localStorage.getItem("seen_"+pop_id) !== "true") {
-    document.getElementById(pop_id).style.display = "block";
+function show_popovers () {
+  for (var i = 0; i < popovers.length; i++) {
+    let pop_id = popovers[i];
+    if (localStorage.getItem("seen_"+pop_id) !== "true") {
+      document.getElementById(pop_id).style.display = "block";
+    }
   }
 }
+
+function drop_popovers () {
+  for (var i = 0; i < popovers.length; i++) {
+    let pop_id = popovers[i];
+    localStorage.setItem("seen_"+pop_id,"none");
+  }
+
+  show_popovers();
+}
+
+show_popovers();
