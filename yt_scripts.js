eb5a14aab8aa9d22b8deaa47c2fe5f975c796c7a
Himjune <himjune@mail.ru>
2020-05-22 20:19:41 +0300

    owl 2205

--- yt_scripts.js
+++ yt_scripts.js
@@ -11,7 +11,7 @@ function onYouTubeIframeAPIReady() {
   if (videoId === '') {
     //videoId = '33ENT8wlROM'; // my test
     //videoId = '5Tw5xhRsonc'; // news stream
-    videoId = 'gUx9-hCzq8g'; // next owl
+    videoId = 'OAXihQaXqEY'; // next owl
     //videoId = 'oEfQawTuAbw' // owl highlights
   }
 
